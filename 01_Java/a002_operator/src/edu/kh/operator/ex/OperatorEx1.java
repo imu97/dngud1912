5cd664425e2193e65c1e29c5d689e3b7f5ab2f2d
imu971216 <dngud1912@gmail.com>
2023-02-08 11:51:38 +0900

    2023/02/08 연산자

---
+++ 01_Java/a002_operator/src/edu/kh/operator/ex/OperatorEx1.java
@@ -0,0 +1,11 @@
+package edu.kh.operator.ex;
+
+import java.util.Scanner;
+
+public class OperatorEx1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+	}
+}
