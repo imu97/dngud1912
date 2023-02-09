1988011a84c92daf40c78e086a8f99f05a2abf5f
imu971216 <dngud1912@gmail.com>
2023-02-09 17:50:17 +0900

    2023/02/09 연습문제

---
+++ 01_Java/a003_control/src/edu/kh/control/condition/practice/ConditionPractice.java
@@ -0,0 +1,40 @@
+package edu.kh.control.condition.practice;
+
+import java.util.Scanner;
+
+public class ConditionPractice {
+
+	public void practice1() {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자를 한 개 입력하세요 : ");
+		
+		int input = sc.nextInt();
+		  
+		if(input % 2 == 0) {
+			System.out.println("짝수입니다.");
+		}else {
+			System.out.println("홀수입니다.");
+		}
+		 
+	
+	
+	
+	
+	
+	
+	
+	}
+	public void practice2() {
+		
+	}
+	public void practice3() {
+		
+	}
+	public void practice4() {
+		
+	}
+	public void practice5() {
+		
+	}
+}
