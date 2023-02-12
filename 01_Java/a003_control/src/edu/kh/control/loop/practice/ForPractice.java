d91431a1c05d4eb0bd47d9ed423fffc778377c9d
dngud1912 <dngud1912@gmail.com>
2023-02-12 18:46:18 +0900

    2023/02/12 문제풀이

--- 01_Java/a003_control/src/edu/kh/control/loop/practice/ForPractice.java
+++ 01_Java/a003_control/src/edu/kh/control/loop/practice/ForPractice.java
@@ -63,20 +63,21 @@ public class ForPractice {
 		}
 		
 		
-		public void practice4() {
-			
-			Scanner sc = new Scanner(System.in);
-			
-			System.out.print("첫 번째 숫자 :");
-			int input1 = sc.nextInt();
-			System.out.print("두 번째 숫자 :");
-			int input2 = sc.nextInt();
-			
-			if(input2 < 0) {
-				System.out.print("1 이상으; 숫자를 입력해주세요.");
-			}
-		}
+//		public void practice4() {
+//			
+//			Scanner sc = new Scanner(System.in);
+//			
+//			System.out.print("첫 번째 숫자 :");
+//			int input1 = sc.nextInt();
+//			System.out.print("두 번째 숫자 :");
+//			int input2 = sc.nextInt();
+//			
+//			if(input1 < 0 && input2 < 0) {
+//				System.out.print("1 이상의 숫자를 입력해주세요.");
+//			}
+//		}
 		
+	
 		public void practice5() {
 			
 			Scanner sc = new Scanner(System.in);
@@ -84,13 +85,49 @@ public class ForPractice {
 			System.out.print("숫자 : ");
 			int input = sc.nextInt();
 			
+			System.out.println("==== " + input + "단 ====");
+			
 			for(int i = 1; i <= 9; i++) {
+				System.out.printf("%d * %d = %d \n", input, i, input * i);
 			}
 		}
 		
 		
+		public void practice6() {
+			
+		}
+		public void practice7() {
+			
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.print("정수 입력 : ");
+			int input = sc.nextInt();
+			
+			for(int x = 1; x <= input; x++ ) {
+				
+				for(int i = 1; i <= x; i++) {
+					System.out.printf("*", i);
+				}
+				System.out.println();
+			}
+		}
 		
 		
+		public void practice8() {
+			
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.print("정수 입력 : ");
+			int input = sc.nextInt();
+			
+			for(int x = input; x >= 1; x--) {
+				
+				for(int i = x; i >= 1; i--) {
+					System.out.printf("*",i);
+				}
+				System.out.println();
+			}
+		} 
 		
 		
 		
