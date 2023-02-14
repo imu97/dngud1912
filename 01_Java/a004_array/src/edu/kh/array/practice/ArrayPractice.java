79499fd2c963c8803c0cac9091ae9799111969cc
dngud1912 <dngud1912@gmail.com>
2023-02-14 22:50:49 +0900

    2023/02/14 저녁에 집에서 문제풀이

--- 01_Java/a004_array/src/edu/kh/array/practice/ArrayPractice.java
+++ 01_Java/a004_array/src/edu/kh/array/practice/ArrayPractice.java
@@ -1,5 +1,7 @@
 package edu.kh.array.practice;
 
+import java.util.Scanner;
+
 public class ArrayPractice {
 
 public void ex1() {
@@ -13,7 +15,12 @@ public void ex1() {
 		for(int i = 1; i <= 9; i++) {
 			System.out.print(i + " ");
 			
+			if(i % 2 == 1) {
+				sum += i;
+			}
 		}
+		System.out.println();
+		System.out.print("짝수 번째 인덱스 합 : " + sum);
 	}
 		
 	
@@ -32,9 +39,51 @@ public void ex1() {
 			}
 		}
 		System.out.println();
-		
 		System.out.print("홀수 번째 인덱스 합 : " + sum);
-		
 	}
 		
+	
+	public void ex3() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("양의 정수 : ");
+		int input = sc.nextInt();
+		
+		for(int i = 1; i <= input; i++) {
+			System.out.print(i + " ");
+		}
+	}
+	
+	
+	public void ex4() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int arr[] = new int[5];
+		int index = 0;	
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.printf("입력 %d : ", i);
+			int input = sc.nextInt();
+			
+		}	
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- 01_Java/a004_array/src/edu/kh/array/practice/ArrayPracticeRun.java
+++ 01_Java/a004_array/src/edu/kh/array/practice/ArrayPracticeRun.java
@@ -6,7 +6,29 @@ public class ArrayPracticeRun {
 		
 		ArrayPractice ap = new ArrayPractice(); 
 				
-		ap.ex1();
+//		ap.ex1();
 //		ap.ex2();
+//		ap.ex3();
+		ap.ex4();
+//		ap.ex5();
+//		ap.ex6();
+//		ap.ex7();
+//		ap.ex8();
+//		ap.ex9();
+//		ap.ex10();
+//		ap.ex11();
+//		ap.ex12();
+//		ap.ex13();
+//		ap.ex14();
+//		ap.ex15();
+//		ap.ex16();
+//		ap.ex17();
+//		ap.ex18();
+//		ap.ex19();
+//		ap.ex20();
+//		ap.ex21();
+//		ap.ex22();
+//		ap.ex23();
+//		ap.ex24();
 	}
 }
